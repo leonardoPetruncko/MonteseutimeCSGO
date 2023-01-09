788d1a6ba2bb844002824f14870d6600c5e98c20
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-01-09 13:32:50 -0300

    Resolvendo bugs

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -61,7 +60,7 @@ function App() {
         nome={time.nome} 
         corPrimaria={time.corPrimaria} 
         corSecundaria={time.corSecundaria} 
-        colaboradores={colaboradores}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
 
       <Rodape />   
