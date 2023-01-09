60dd62c588c9cc79fe76a2e2563ae3778cd56861
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-01-09 04:05:51 -0300

    Card Colaborador

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,7 +1,22 @@
 import './Colaborador.css'
 
 const Colaborador = () => {
-    return ()
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/leonardopetruncko.png' alt='Leonardo'></img>
+            </div>
+
+            <div className='rodape'>
+                <h4>
+                    Leonardo
+                </h4>
+                <h5>
+                    Professor
+                </h5>
+            </div>
+        </div>
+    )
 }
 
 export default Colaborador
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
@@ -7,6 +8,7 @@ const Time = (props) => {
     return (
         <section className='time' style={{backgroundColor: props.corSecundaria}}>
             <h3 style= {css} >{props.nome}</h3>
+            <Colaborador />
         </section>
     )
 }
