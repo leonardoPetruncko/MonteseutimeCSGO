9dedc6131bd9c446f5650c2e60b25bb03fa510e1
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-02-02 15:35:53 -0300

    Atualizando Form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -28,18 +28,18 @@ const Formulario = (props) => {
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <h2>Preencha para criar os Cards dos Jogadores</h2>
                 <CampoTexto 
                     obrigatorio={true}
-                    label="Nome"
-                    placeholder="Digite seu nome" 
+                    label="Jogador"
+                    placeholder="Digite o Jogador" 
                     valor={nome}
                     aoAlterado={valor => setNome(valor)}
                 />
                 <CampoTexto
                     obrigatorio={true}
-                    label="Cargo"
-                    placeholder="Digite seu cargo" 
+                    label="Função"
+                    placeholder="Digite sua Função dentro do time" 
                     valor={cargo}
                     aoAlterado={valor => setCargo(valor)}
                 />
@@ -51,7 +51,7 @@ const Formulario = (props) => {
                 />
                 <ListaSuspensa
                     obrigatorio={true}
-                    label="Time" 
+                    label="Digite sua Rota" 
                     itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
